e3914376fbff108d163b679d5adda040a8b64021
totallyfake <maporter104@gmail.com>
2018-03-19 21:56:04 -0500

    moved spdlog to module and fixed stuff

--- src/loggersetup.cpp
+++ src/loggersetup.cpp
@@ -12,7 +12,7 @@ namespace vtk {
 
 	void LoggerSetup::setup() {
 		std::vector<spdlog::sink_ptr> logSinks;
-		logSinks.push_back(std::make_shared<spdlog::sinks::rotating_file_sink_mt>("logs/log", "log", 1048576 * 5, 3));
+		logSinks.push_back(std::make_shared<spdlog::sinks::rotating_file_sink_mt>("logs/log", 1048576 * 5, 3));
 		logSinks.push_back(std::make_shared<spdlog::sinks::stdout_sink_st>());
 		auto generalLogger = std::make_shared<spdlog::logger>("general", begin(logSinks), end(logSinks));
 		auto fileLogger = std::make_shared<spdlog::logger>("file", begin(logSinks), end(logSinks));
